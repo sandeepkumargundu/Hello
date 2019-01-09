076fe23d72ba21b3500ff5dbf0fde7788b75dd3e
skgundu <kumar.sandeep528@gmail.com>
2019-01-09 17:53:12 +0530

    add Hello program

---
+++ Test1.java
@@ -0,0 +1,13 @@
+public class Test1 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		for(int i = 0; i <= 10; i++)
+		{
+			System.out.println("This is first Test... " + i);
+		}		
+	}
+
+}
